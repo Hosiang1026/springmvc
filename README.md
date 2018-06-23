e220f25130d07c8ea3d3d9cbe8ab8dabd3745039
Howe Hsiang <hosiang969@sina.com>
2018-06-23 18:21:34 +0800

    test springmvc

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # springmvc
-This a simple springmvc project.
+
+☆项目名称：springmvc
+
+☆JDK版本：JDK 1.8
+
+☆运行环境：apache-tomcat-9.0.8
+
+☆开发工具：IntelliJ IDEA 2018.1.5
+
+
--- src/main/java/com/study/TestController.java
+++ src/main/java/com/study/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
- 
 /**
  * 测试
  * 
@@ -20,11 +19,17 @@ public class TestController {
     @RequestMapping("/hello")
     public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "Howe Hsiang") String name) {
 
-        //指定视图
+        //1、收集参数、验证参数
+        //2、绑定参数到命令对象
+        //3、将命令对象传入业务对象进行业务处理
+        //4、选择下一个页面
+
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
         ModelAndView mv = new ModelAndView("hellospring");
-        //向视图中添加所要展示或使用的内容，将在页面中使用
+        //添加模型数据 可以是任意的POJO对象
         mv.addObject("message", message);
         mv.addObject("name", name);
         return mv;
     }
+
 }
