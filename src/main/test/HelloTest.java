6bd2a908e607a774459899158fb610f2aea88720
Howe Hsiang <hosiang969@sina.com>
2018-06-23 20:01:27 +0800

    Test SpringMVC

---
+++ src/main/test/HelloTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * 测试类
+ */
+public class HelloTest extends BaseTest {
+
+    @Test
+    public void getHelloInfo() {
+        System.out.println("hello");
+    }
+
+}
+
+
+
+
